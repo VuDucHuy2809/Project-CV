529b0b72f8f03bfa35b314a1dd399f2f7b24aff3
Đức Huy <VuDucHuy2809@gmail.com>
2023-03-18 20:41:53 +0700

    update

---
+++ Đồ án Mã Nguồn Mở/ecommerceu/backend/data.js
@@ -0,0 +1,60 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'FORMAL SHIRT1',
+            slug: 'formal-shirt',
+            category:'Shirts',
+            image:'/images/22.jpg',
+            price: 2501,
+            countInStock: 10,
+            brand: "Men",
+            numReviews: 10,
+            rating: 5,
+            description: 'Cutaway Non-Iron Cornflower Blue'
+
+        },
+        {
+            _id: '2',
+            name: 'FORMAL SHIRT2',
+            slug: 'formal-shirt',
+            category:'Shirts',
+            image:'/images/25.jpg',
+            price: 2502,
+            countInStock: 10,
+            brand: "Woman",
+            numReviews: 10,
+            rating: 5,
+            description: 'Cutaway Non-Iron Cornflower Blue'
+
+        },
+        {
+            _id: '3',
+            name: 'FORMAL SHIRT3',
+            slug: 'formal-shirts',
+            category:'Shirts',
+            image:'/images/26.jpg',
+            price: 2503,
+            countInStock: 10,
+            brand: "Men",
+            numReviews: 10,
+            rating: 2.5,
+            description: 'Cutaway Non-Iron Cornflower Blue'
+
+        },
+        {
+            _id: '4',
+            name: 'FORMAL SHIRT4',
+            slug: 'formal-shirts',
+            category:'Shirts',
+            image:'/images/27.jpg',
+            price: 2504,
+            countInStock: 10,
+            brand: "Men",
+            numReviews: 10,
+            rating: 5,
+            description: 'Cutaway Non-Iron Cornflower Blue'
+        }
+    ]
+}
+export default data
